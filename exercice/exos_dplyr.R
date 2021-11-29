caa8251e7ece8dd584a74723110fedabbdf4dad9
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2021-11-29 17:36:22 +0100

    correct the label

--- exercice/exos_dplyr.R
+++ exercice/exos_dplyr.R
@@ -3,5 +3,5 @@
 dat <- data_speeco_list()
 
 png(here::here("outputs", "exo_dplyr_hist.png"))
-hist(table(dat$ecoregion_id), xlab = "number of species", ylab = "number of ecoregins", col ="blue")
+hist(table(dat$ecoregion_id), xlab = "number of species", ylab = "number of ecoregions", col ="blue")
 dev.off()
